c019af70c97c59bd2ca4777dabeb4b6feda59d43
MarcosIraola <iraolamarcos@gmail.com>
2022-10-07 19:31:05 -0300

    disabled generic button

--- src/components/ui/Button/TButton.jsx
+++ src/components/ui/Button/TButton.jsx
@@ -3,10 +3,12 @@ import styles from './TButton.module.css'
 
 const TButton = (props) => {
 
-    const {title, function_1} = props;
+    const {title, function_1, disabled} = props;
+
+    console.log(disabled)
 
     return (
-        <div className={styles.container} onClick={function_1}>
+        <div className={disabled ? styles.disabled : styles.container} onClick={!disabled ? function_1 : undefined}>
             <p className={styles.text}>{title}</p>
         </div>
     )
--- src/pages/Start/Start.jsx
+++ src/pages/Start/Start.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from './Start.module.css'
+import TButton from '../../components/ui/Button/TButton'
 
 const Start = () => {
+
+    const [name, setName] = useState('')
+
     return (
-        <div>Exams</div>
+        <div className={styles.container}>
+            <p className={styles.title}>Please write your name</p>
+            <input 
+                className={styles.input} 
+                type="text" 
+                placeholder='Your name' 
+                onInput={e => setName(e.target.value)}
+            />
+            <div className={styles.button}>
+                <TButton title='Continue' function_1={() => console.log(name)} disabled={!name}/>
+            </div>
+        </div>
     )
 }
 
