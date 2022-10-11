15a57bb2fd72a080ae7ded790eb8908d5766eaac
MarcosIraola <iraolamarcos@gmail.com>
2022-10-11 20:38:26 -0300

    api, examview

--- src/pages/ExamView/ExamView.jsx
+++ src/pages/ExamView/ExamView.jsx
@@ -1,18 +1,35 @@
 import React from 'react'
+import TButton from '../../components/ui/Button/TButton'
 import styles from './ExamView.module.css'
 
 const ExamView = () => {
     return (
         <div className={styles.container}>
-            <h1>Evaluacion Full Stack Developer</h1>
-            <p>
-                En esta evaulacion buscamos evaluar tu senority. 
-                El resultado de la evaluación lo vas a poder ver una vez terminada la misma.
-            </p>
+            <div className={styles.info_container}>
+                <h1>Evaluacion Full Stack Developer</h1>
+                <p>
+                    En esta evaulacion buscamos evaluar tu senority. 
+                    El resultado de la evaluación lo vas a poder ver una vez terminada la misma.
+                </p>
+            </div>
             <div className={styles.questions_container}>
                 <div>
                     <h2>Verdadero o falso: HTML es un lenguaje de programación.</h2>
+                    <span>Verdadero | Falso</span>
                 </div>
+                <div>
+                    <h2>Verdadero o falso: HTML es un lenguaje de programación.</h2>
+                    <p>Opcion 1</p>
+                    <p>Opcion 2</p>
+                    <p>Opcion 3</p>
+                </div>
+                <div>
+                    <h2>Verdadero o falso: HTML es un lenguaje de programación.</h2>
+                    <textarea>Your answer here...</textarea>
+                </div>
+            </div>
+            <div className={styles.button_container}>
+                <TButton title='Submit'/>
             </div>
         </div>
     )
--- src/pages/Start/Start.jsx
+++ src/pages/Start/Start.jsx
@@ -1,28 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './Start.module.css'
 import TButton from '../../components/ui/Button/TButton'
 import ExamCard from '../../components/ExamsCard/ExamCard'
+import { ExpandAltOutlined } from '@ant-design/icons'
 
 const Start = () => {
 
-    const [name, setName] = useState('')
+    const [search, setSearch] = useState('')
+    const [exams, setExams] = useState([]);
+    const [filteredExams, setFilteredExams] = useState([])
+
+    useEffect(() => {
+        const url = 'http://127.0.0.1:5432/exams';
+        const options = {
+            method: 'GET',
+            headers: {"Content-type": "application/json"},
+        };
+        fetch(url, options)
+            .then(response => response.json())
+            .then(payload => {
+                setExams(payload.exams);
+            })
+            .catch(error => {
+                console.log(error)
+            })
+
+    }, []);
+
+    useEffect(() => {
+        
+    }, [search])
 
     return (
         <div className={styles.container}>
-            {/* <p className={styles.title}>Please write your name</p>
-            <input 
-                className={styles.input} 
-                type="text" 
-                placeholder='Your name' 
-                onInput={e => setName(e.target.value)}
-            />
-            <div className={styles.button}>
-                <TButton title='Continue' function_1={() => console.log(name)} disabled={!name}/>
-            </div> */}
-            <h1>Welcome, `user`</h1>
+            <h1>Welcome!</h1>
             <div className={styles.completed_container}>
                 <h2 className={styles.title}>Completed Exams</h2>
-                <span>You have 0 exams completed.</span>
                 <ExamCard title='Math' description='Lorem impsum titus a griad du bere th december!' completed/>
             </div>
             <div className={styles.incompleted_container}>
@@ -32,12 +45,19 @@ const Start = () => {
                         className={styles.input} 
                         type="text" 
                         placeholder='&#xF002; Search..' 
-                        onInput={e => setName(e.target.value)}
+                        onInput={e => setSearch(e.target.value)}
                     />
                 </div>
-                <ExamCard title='Math' description='Lorem impsum titus a griad du bere th december!' id='1'/>
-                <ExamCard title='Math' description='Lorem impsum titus a griad du bere th december!' id='2'/>
-                <ExamCard title='Math' description='Lorem impsum titus a griad du bere th december!' id='3'/>
+                {
+                    exams?.map((exam, index) => (
+                        <ExamCard
+                            key={index}
+                            id={exam.id} 
+                            title={exam.title} 
+                            description={exam.description}
+                        />
+                    ))
+                }
             </div>
 
         </div>
